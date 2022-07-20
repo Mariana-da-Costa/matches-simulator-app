66f84e526b97dd31faf39999b8f52b6ccca260c9
Mariana da Costa <marianacosta.engc@gmail.com>
2022-07-19 23:45:09 -0300

    Criado Layouts do App

---
+++ Simulator/app/src/main/java/me/mariana/simulator/DetailActivity.kt
@@ -0,0 +1,17 @@
+package me.mariana.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.mariana.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
