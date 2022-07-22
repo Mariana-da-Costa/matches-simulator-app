85db145106e89797d9a881143dfec5a0316ed616
Mariana da Costa <marianacosta.engc@gmail.com>
2022-07-21 22:16:55 -0300

    Melhorias no código

--- Simulator/app/src/main/java/me/mariana/simulator/MainActivity.kt
+++ Simulator/app/src/main/java/me/mariana/simulator/MainActivity.kt
@@ -10,9 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //setContentView(R.layout.activity_main)
-        //val tvHello = findViewById<TextView>(R.id.tvHello)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
